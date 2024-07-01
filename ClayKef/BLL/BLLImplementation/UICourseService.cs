d0016823e1d2300e51f15213f87cd7702114ce3e
ShiraHR <h65246@gmail.com>
2024-07-01 18:08:35 +0300

    adding getMember method to courseToMemberRepo

--- ClayKef/BLL/BLLImplementation/UICourseService.cs
+++ ClayKef/BLL/BLLImplementation/UICourseService.cs
@@ -114,24 +114,24 @@ namespace BLL.BLLImplementation
             return newCourse;
         }
 
-        /*        public List<UICourse> GetFilteredCourses(BaseQueryParams queryParams)
-                {
-                    List<Course> courseTask = courseRepo.Get(queryParams);
-                    var CourseList = new List<UICourse>();
-                    foreach (Course course in courseTask)
-                    {
-                        UICourse newCourse = new();
-                        newCourse.Name = course.Name;
-                        newCourse.Ageing = course.AgeCodeNavigation.Name;
-                        newCourse.Level = course.CourseLevelCodeNavigation.Type;
-                        newCourse.Price = cour se.PricingCodeNavigation.Price;
-                        newCourse.Day = course.TimingCodeNavigation.Day;
-                        newCourse.Hour = (float)course.TimingCodeNavigation.Hour;
-                        newCourse.NumOfMembers = course.NumOfMembers;
-                        CourseList.Add(newCourse);
-                    }
-                    return CourseList;
-                }*/
+        /*public List<UICourse> GetFilteredCourses(BaseQueryParams queryParams)
+        {
+            List<Course> courseTask = courseRepo.Get(queryParams);
+            var CourseList = new List<UICourse>();
+            foreach (Course course in courseTask)
+            {
+                UICourse newCourse = new();
+                newCourse.Name = course.Name;
+                newCourse.Ageing = course.AgeCodeNavigation.Name;
+                newCourse.Level = course.CourseLevelCodeNavigation.Type;
+                newCourse.Price = cour se.PricingCodeNavigation.Price;
+                newCourse.Day = course.TimingCodeNavigation.Day;
+                newCourse.Hour = (float)course.TimingCodeNavigation.Hour;
+                newCourse.NumOfMembers = course.NumOfMembers;
+                CourseList.Add(newCourse);
+            }
+            return CourseList;
+        }*/
     }
 }
 
--- ClayKef/BLL/BLLImplementation/UIMemberService.cs
+++ ClayKef/BLL/BLLImplementation/UIMemberService.cs
@@ -37,4 +37,3 @@ namespace BLL.BLLImplementation
         }
     }
 }
- 
